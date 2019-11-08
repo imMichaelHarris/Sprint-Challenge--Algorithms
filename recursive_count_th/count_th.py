032ec205b288430dde820c0eb758623577821321
Michael Harris <mhguitar1997@gmail.com>
2019-11-08 09:19:49 -0800

    Count checks if th is in word

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,11 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    # First find if the word has one occurence if it does run count_th again
+    #
+    if "th" in word:
+        print("yes")
+    else:
+        print("no")
+
+count_th("this")
