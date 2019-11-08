078b8e1da66e64f80952aee67433ec3a4b9367f2
Michael Harris <mhguitar1997@gmail.com>
2019-11-08 09:32:41 -0800

    Got a plan need to execute

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,10 +7,18 @@ def count_th(word):
     
     # TBC
     # First find if the word has one occurence if it does run count_th again
-    #
+    # Base case will be when there are no more "th" in a word
+    # When base case is met retuirn the number
+
+    #Need to figure out how to remove only the first occurence of th for the recuresive call
+    count = 0
     if "th" in word:
-        print("yes")
+        count += 1
+        print("yes", count)
     else:
         print("no")
+        return 
 
-count_th("this")
+    count_th("this")
+
+count_th("thisth")
